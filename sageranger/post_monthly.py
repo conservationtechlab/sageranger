36576079c3bd844a97888b0b8071988039480288
Aslan Li <83924283+uuuhei@users.noreply.github.com>
2023-07-10 15:35:48 -0700

    Update post_monthly.py
    changed the way of identifying camera traps

--- sageranger/post_monthly.py
+++ sageranger/post_monthly.py
@@ -6,7 +6,7 @@ and uses an http request to post an empty observation with longitude and
 latitude (0,0) once every month with cougar vision to ensure the camera stays
 active on sagebrush.
 '''
-from datetime import datetime as dt
+from datetime import datetime
 import requests
 
 
@@ -20,32 +20,39 @@ def post_monthly_obs(token, auth):
     Returns: the http request response code to tell us if the call worked
     or not
     '''
-    headers = {
+    hdr = {
         'X-CSRFToken': token,
         'Authorization': auth,
         'Accept': 'application/json'
     }
 
-    url_obs = 'https://sagebrush.pamdas.org/api/v1.0/observations/'
-    url_s = 'https://sagebrush.pamdas.org/api/v1.0/sources/'
-
-    current_time = dt.utcnow()
+    url = 'https://sagebrush.pamdas.org/api/v1.0/observations/'
+    url_2 = 'https://sagebrush.pamdas.org/api/v1.0/subjects/'
+    url_3 = 'https://sagebrush.pamdas.org/api/v1.0/subject/'
+    current_time = datetime.utcnow()
     formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
 
-    response = requests.get(url_s, headers=headers, timeout=10)
+    response = requests.get(url_2, headers=hdr, timeout=10)
     response_json = response.json()
-    results = response_json['data']['results']
+    results = response_json['data']
 
     for i in enumerate(results):
         i = i[0]
-        if results[i]['manufacturer_id'][:2] == 'B0':
-            obs_data = {
-                "location": {"longitude": 0, "latitude": 0},
-                "recorded_at": formatted_time,
-                "source": results[i]['id'],
-                "device_status_properties":
-                [{"value": 'test', "label": "animal", "units": ""}],
-                "additional": {"animal": 'test'}
-                }
-            response = requests.post(url_obs, headers=headers, json=obs_data, timeout=10)
-            print(response)
+        if results[i]['subject_subtype'] == 'camera_trap':
+            subject_id = results[i]['id']
+            manu_id = results[i]['name']
+            print(manu_id)
+            url_3 = url_3 + subject_id + '/sources/'
+            response = requests.get(url_3, headers=hdr, timeout=10)
+            response_json = response.json()
+            if response_json['data'] != []:
+                source_id = response_json['data'][0]['id']
+                data = {
+                        "location": {"longitude": 0, "latitude": 0},
+                        "recorded_at": formatted_time,
+                        "source": source_id,
+                        "device_status_properties":
+                        [{"value": 'test', "label": "animal", "units": ""}],
+                        "additional": {"animal": 'test'}}
+                obs = requests.post(url, headers=hdr, json=data, timeout=10)
+                print(obs)
