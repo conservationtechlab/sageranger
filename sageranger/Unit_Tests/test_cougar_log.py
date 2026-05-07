0ca47455cf6afb0a096077ab4300fe33101dca93
Montserrat Jara <jaramontse3@gmail.com>
2026-05-07 12:37:24 -0700

    Fix pylint and flake8 errors.

--- sageranger/Unit_Tests/test_cougar_log.py
+++ sageranger/Unit_Tests/test_cougar_log.py
@@ -1,10 +1,20 @@
+"""Test cougar log
+
+Usage:
+    python3 Unit_Tests.test_cougar_log
+
+This function test creating observations in
+earthranger for the specifed camera.
+"""
+
 from post_cougar_log import is_target
 
 
 def main():
-   """Tests creating an observation in earth ranger"""
-   
-   is_target("<camera name>","<token?", "<label>")
+    """Tests creating an observation in earth ranger"""
+
+    is_target("<camera name>", "<token?", "<label>")
+
 
 if __name__ == "__main__":
     main()
