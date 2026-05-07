e536bdf52e854785cd5c490ec7001601e09d4642
Montserrat Jara <jaramontse3@gmail.com>
2026-05-07 12:40:10 -0700

    Fix flake8 and pylint errors.

--- sageranger/Unit_Tests/test_locations.py
+++ sageranger/Unit_Tests/test_locations.py
@@ -1,10 +1,20 @@
+"""Tests camera locations
+
+Usage:
+    python3 -m UnitTests.test_locations
+
+Make sure you are testing from the sageranger
+root folder.
+"""
+
 from get_cam_location import cam_location
 
+
 def main():
-   """Tests get cam locations"""
-   coordinates, id = cam_location("<cam name>", "<token>")
-   print("Coordintes and ID:", coordinates, id)
-      
+    """Tests get cam locations"""
+    coordinates, s_id = cam_location("<cam name>", "<token>")
+    print("Coordintes and ID:", coordinates, s_id)
+
 
 if __name__ == "__main__":
     main()
