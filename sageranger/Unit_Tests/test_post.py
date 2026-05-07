2cbabf7dc3279525e44940521628bc5a696cad8c
Montserrat Jara <jaramontse3@gmail.com>
2026-05-07 13:31:32 -0700

    Fix flake8 and pylint errors.

--- sageranger/Unit_Tests/test_post.py
+++ sageranger/Unit_Tests/test_post.py
@@ -1,11 +1,21 @@
+"""Tests Attach Images
+
+Usage:
+    python3 -m UnitTests.test_post
+
+This function tests posting an event 
+on earthranger. Make sure to test from
+the sageranger root folder.
+"""
+
 from post_event_er import post_event
 
 def main():
-   """Tests creating an event"""
-   
-   event_id = post_event("<label>","<cam name>", "<token>")
-   print("Event_ID:", event_id)
-    
+    """Tests creating an event"""
+
+    event_id = post_event("<label>","<cam name>", "<token>")
+    print("Event_ID:", event_id)
+
 
 if __name__ == "__main__":
     main()
