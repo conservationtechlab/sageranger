255a9bf7ede39fb5bf0e883b42dc741991dbeb27
Montserrat Jara <jaramontse3@gmail.com>
2026-05-19 13:47:19 -0700

    Fix pylint and flake8 errors.

--- sageranger/Unit_Tests/test_cougar_log.py
+++ sageranger/Unit_Tests/test_cougar_log.py
@@ -9,16 +9,17 @@ earthranger for the specifed camera.
 
 from post_cougar_log import is_target
 
-CAMERA = "<camera name>" 
+CAMERA = "<camera name>"
 TOKEN = "<bearer token>"
 LABEL = "<animal>"  # ex. cougar, bobcat
 
+
 def main():
     """Tests creating an observation in earth ranger
-    
+
     This function uses global variables to call the is_
-    target function. This function has no return 
-    or print statement. To verify results check for 
+    target function. This function has no return
+    or print statement. To verify results check for
     historical data under the selected camera icon
     in earthranger.
 
