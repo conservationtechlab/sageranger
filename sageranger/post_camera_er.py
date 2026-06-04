041c6f6797b73c4b77c62dcdf320ad1daec64807
Montserrat Jara <jaramontse3@gmail.com>
2026-06-04 10:13:56 -0700

    Fix pylint/flake8 errors and replace file path.

--- sageranger/post_camera_er.py
+++ sageranger/post_camera_er.py
@@ -5,7 +5,7 @@ in Earthranger that shows up on the map and returns the id of the camera trap.
 
 takes in a list of names and coordinates of new camera traps,
 posts the camera traps as sources and subjects on Earthranger as well as an
-intial observation, and returns the source and subject id of the camera 
+intial observation, and returns the source and subject id of the camera
 traps uploaded.
 
 Inputs:
@@ -41,7 +41,9 @@ hdr = {
 
 URL = 'https://sagebrush.pamdas.org/api/v1.0/'
 
-df = pd.read_csv('/home/montse/sageranger/sageranger/camera_test.csv', delimiter=' ', header=0)
+df = pd.read_csv('/path/to/csv',
+                 delimiter=' ',
+                 header=0)
 cam = df.camera.tolist()
 lat = df.lat.tolist()
 longi = df.longi.tolist()
@@ -51,7 +53,7 @@ for i in enumerate(cam):
     current_time = datetime.now(UTC)
     formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
 
-    # first create a source 
+    # first create a source
     payload = {
         "source_type": "seismic",
         "manufacturer_id": cam[i],
@@ -65,8 +67,8 @@ for i in enumerate(cam):
         "assigned_range": {}
     }
 
-    URL_2 = URL + 'sources/'
-    source = requests.post(URL_2, headers=hdr, json=payload, timeout=10)
+    url_2 = URL + 'sources/'
+    source = requests.post(url_2, headers=hdr, json=payload, timeout=10)
     response_js = source.json()
     source_id = response_js['data']['id']
 
@@ -82,8 +84,8 @@ for i in enumerate(cam):
         "is_active": 1,
         }
 
-    URL_3 = URL + 'subjects/'
-    subject = requests.post(URL_3, headers=hdr, json=payload, timeout=10)
+    url_3 = URL + 'subjects/'
+    subject = requests.post(url_3, headers=hdr, json=payload, timeout=10)
     subject_js = subject.json()
     subject_id = subject_js['data']['id']
 
@@ -93,15 +95,14 @@ for i in enumerate(cam):
         "source": source_id,
         "source_type": "tracking-device",
         "additional": {},
-        "location":{
-        "latitude": lat[i],
-        "longitude": longi[i]
-        }
+        "location": {
+            "latitude": lat[i],
+            "longitude": longi[i]}
     }
 
-    URL_4 = URL + 'subject/'+ subject_id+'/sources/'
-    requests.post(URL_4, headers=hdr, json=payload, timeout=10)
-    response = requests.get(URL_4, headers=hdr, timeout=10)
+    url_4 = URL + 'subject/' + subject_id+'/sources/'
+    requests.post(url_4, headers=hdr, json=payload, timeout=10)
+    response = requests.get(url_4, headers=hdr, timeout=10)
     source_2 = response.json()
 
     # post a test observation to put camera on the map
@@ -110,20 +111,22 @@ for i in enumerate(cam):
     payload = {
             "location": {
                 "longitude": 0,
-                  "latitude": 0},
+                "latitude": 0
+            },
             "recorded_at": formatted_time,
             "source": source_id,
-             "device_status_properties":[{
-                 "value": "test", 
-                 "label": "animal", 
-                 "units": ""}],
+            "device_status_properties": [{
+                 "value": "test",
+                 "label": "animal",
+                 "units": ""
+                 }],
             "additional": {
                 "animal": 'test'}
      }
-    
-    URL_5 = URL + 'observations/'
-    obs = requests.post(URL_5, headers=hdr, json=payload, timeout=10)
-    
+
+    url_5 = URL + 'observations/'
+    obs = requests.post(url_5, headers=hdr, json=payload, timeout=10)
+
     print("\nsubject id: " + subject_id)
     print("source id: " + source_2['data'][0]['id'])
     print("camera trap " + cam[i] + " is uploaded to sagebrush\n")
