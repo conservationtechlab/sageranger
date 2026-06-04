7806757de83e0acf0decb6bc5341d7e62092d9bf
Montserrat Jara <jaramontse3@gmail.com>
2026-06-04 09:56:57 -0700

    Fix url order numbers and adjust module docstring.

--- sageranger/post_camera_er.py
+++ sageranger/post_camera_er.py
@@ -1,28 +1,31 @@
-'''Post Camera ER
+"""Post Camera ER
 
 This module defines a script called post-camera which creates new cameras
 in Earthranger that shows up on the map and returns the id of the camera trap.
 
 takes in a list of names and coordinates of new camera traps,
-posts the camera traps as sources and subjects on Earthranger, and returns
-the source and subject id of the camera traps uploaded.
+posts the camera traps as sources and subjects on Earthranger as well as an
+intial observation, and returns the source and subject id of the camera 
+traps uploaded.
 
 Inputs:
-authorization: the other auth token for ER as defined in config yml, this
-was retrieved from the interactive api on ER
-https://<YOUR INSTANCE>.pamdas.org/api/v1.0/docs/interactive/
-csv: a csv file that contains camera name, longitude and latitude of the
-camera trap with header ['camera', 'lat', 'long'] and white space as
-delimiter. Use the following as a sample:
-           camera lat long
-           S010 0 0
-           S020 0 0
-           S030 0 0
-           S040 0 0
-Outputs:
-prints out the subject id and source id of the uploaded camera trap.
 
-'''
+    authorization: the other auth token for ER as defined in config yml, this
+        was retrieved from the interactive api on ER
+        https://<YOUR INSTANCE>.pamdas.org/api/v1.0/docs/interactive/
+    csv: a csv file that contains camera name, longitude and latitude of the
+        camera trap with header ['camera', 'lat', 'long'] and white space as
+        delimiter. Use the following as a sample:
+             camera lat long
+            S010 0 0
+            S020 0 0
+            S030 0 0
+            S040 0 0
+Outputs:
+
+    prints out the subject id and source id of the uploaded camera trap.
+
+"""
 
 from datetime import datetime, UTC
 import requests
@@ -63,8 +66,8 @@ for i in enumerate(cam):
         "assigned_range": {}
     }
 
-    URL_3 = URL + 'sources/'
-    source = requests.post(URL_3, headers=hdr, json=payload, timeout=10)
+    URL_2 = URL + 'sources/'
+    source = requests.post(URL_2, headers=hdr, json=payload, timeout=10)
     response_js = source.json()
     source_id = response_js['data']['id']
 
@@ -79,8 +82,8 @@ for i in enumerate(cam):
         "is_active": 1,
         }
 
-    URL_2 = URL + 'subjects/'
-    subject = requests.post(URL_2, headers=hdr, json=payload, timeout=10)
+    URL_3 = URL + 'subjects/'
+    subject = requests.post(URL_3, headers=hdr, json=payload, timeout=10)
     subject_js = subject.json()
     subject_id = subject_js['data']['id']
     print("\nsubject id: " + subject_id)
@@ -101,15 +104,21 @@ for i in enumerate(cam):
     response = requests.get(URL_4, headers=hdr, timeout=10)
     source_2 = response.json()
 
-    data = {
-            "location": {"longitude": 0, "latitude": 0},
+    payload = {
+            "location": {
+                "longitude": 0,
+                  "latitude": 0},
             "recorded_at": formatted_time,
             "source": source_id,
-             "device_status_properties":
-            [{"value": 'test', "label": "animal", "units": ""}],
-            "additional": {"animal": 'test'}
-            }
+             "device_status_properties":[{
+                 "value": 'test', 
+                 "label": "animal", 
+                 "units": ""}],
+            "additional": {
+                "animal": 'test'}
+     }
+    
     URL_5 = URL + 'observations/'
-    obs = requests.post(URL_5, headers=hdr, json=data, timeout=10)
+    obs = requests.post(URL_5, headers=hdr, json=payload, timeout=10)
     print('source id: ' + source_2['data'][0]['id'])
     print('camera trap ' + cam[i] + ' is uploaded to sagebrush\n')
