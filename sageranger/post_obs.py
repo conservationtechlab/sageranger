d0571d7fae6d0c3910d1329dc15b2a70de97e53c
Montserrat Jara <jaramontse3@gmail.com>
2026-06-09 13:51:47 -0700

    Add docstrings to file and function.

--- sageranger/post_obs.py
+++ sageranger/post_obs.py
@@ -1,6 +1,31 @@
+"""Posts Observations
+
+This file is responsible for posting 
+an observation to earthranger. It is 
+used by post cougar_log, post_camera,
+and post_monthly.
+
+"""
+
 import requests
 
 def post_observation(subject_id, label, time, hdr):
+    """ Post Observation
+
+    This function posts an observation using the subject_id,
+    label of animal, time of observation, and the authorization
+    token.
+
+    Args:
+        subject_id (str): the id of the subject from earthranger
+        label (str): the name of the animal identified
+        time (str): formatted time of the observation
+        hdr (dict): authorization and permission to access earthrangers
+            api (obtained from the interactive api 
+            https://<YOUR INSTANCE>.pamdas.org/api/v1.0/docs/interactive/ )
+
+    """
+
     url = 'https://sagebrush.pamdas.org/api/v1.0/' 
 
     url_3 = url + 'subject/' + subject_id + '/sources/' 
@@ -21,3 +46,6 @@ def post_observation(subject_id, label, time, hdr):
     obs = requests.post(url_5, headers=hdr, json=payload, timeout=20)
     print("Observation response: ", obs)
     
+
+
+
