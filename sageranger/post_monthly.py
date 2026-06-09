0df3d6b278c7ba6c546e7cfc1b63f32adeb9df29
Montserrat Jara <jaramontse3@gmail.com>
2026-06-09 11:33:27 -0700

    Fix URL to post observation and enhance if condition.

--- sageranger/post_monthly.py
+++ sageranger/post_monthly.py
@@ -6,7 +6,7 @@ and uses an http request to post an empty observation with longitude and
 latitude (0,0) once every month with cougar vision to ensure the camera stays
 active on sagebrush.
 '''
-from datetime import datetime
+from datetime import datetime, UTC
 import requests
 
 
@@ -25,35 +25,43 @@ def post_monthly_obs(token, auth):
         'Authorization': auth,
         'Accept': 'application/json'
     }
-
-    url = 'https://sagebrush.pamdas.org/api/v1.0/observations/'
+    url_1= 'https://sagebrush.pamdas.org/api/v1.0/observations/'
     url_2 = 'https://sagebrush.pamdas.org/api/v1.0/subjects/'
-    url_3 = 'https://sagebrush.pamdas.org/api/v1.0/subject/'
-    current_time = datetime.utcnow()
+    url = 'https://sagebrush.pamdas.org/api/v1.0/subject/'
+
+    current_time = datetime.now(UTC)
     formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
 
     response = requests.get(url_2, headers=hdr, timeout=10)
     response_json = response.json()
     results = response_json['data']
 
+    
     for i in enumerate(results):
         i = i[0]
         if results[i]['subject_subtype'] == 'camera_trap':
             subject_id = results[i]['id']
             manu_id = results[i]['name']
-            print(manu_id)
-            url_3 = url_3 + subject_id + '/sources/'
+            print("name: ", manu_id)
+            
+
+            url_3 = url + subject_id + '/sources/' 
             response = requests.get(url_3, headers=hdr, timeout=10)
             response_json = response.json()
+            # clear after every get request 
+            url_3 = ''
 
-            if response_json['status']['message'] != 'Not Found':
+            if (response_json['status']['message'] != 'Not Found' 
+                and response_json['data'] != []):
                 source_id = response_json['data'][0]['id']
                 data = {
-                        "location": {"longitude": 0, "latitude": 0},
+                        "location": {"longitude": 0, 
+                                     "latitude": 0},
                         "recorded_at": formatted_time,
                         "source": source_id,
                         "device_status_properties":
-                        [{"value": 'test', "label": "animal", "units": ""}],
-                        "additional": {"animal": 'test'}}
-                obs = requests.post(url, headers=hdr, json=data, timeout=10)
+                        [{"value": "test", "label": "animal", "units": ""}],
+                        "additional": {}
+                        }
+                obs = requests.post(url_1, headers=hdr, json=data, timeout=10)
                 print(obs)
