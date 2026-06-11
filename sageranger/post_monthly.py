4899a5c66af31ee2a84260ecd94e1248240ceae1
Montserrat Jara <jaramontse3@gmail.com>
2026-06-11 09:40:36 -0700

    Add parameter camera list to post monthly.

--- sageranger/post_monthly.py
+++ sageranger/post_monthly.py
@@ -11,7 +11,7 @@ import requests
 from post_obs import post_observation
 
 
-def post_monthly_obs(token, auth):
+def post_monthly_obs(token, auth, list_cam):
     '''post_monthly_obs
     This function gets the subject id of camera traps and posts observations
     onto earthranger
@@ -28,31 +28,37 @@ def post_monthly_obs(token, auth):
     }
 
     url = 'https://sagebrush.pamdas.org/api/v1.0/subject/'
-    url_2 = 'https://sagebrush.pamdas.org/api/v1.0/subjects/'
+    url_2 = 'https://sagebrush.pamdas.org/api/v1.0/subjects/?name='
 
     current_time = datetime.now(UTC)
     formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
 
-    response = requests.get(url_2, headers=hdr, timeout=10)
-    response_json = response.json()
-    results = response_json['data']
+    cam_name = list(list_cam.values())
 
-    for i in enumerate(results):
+    for i in enumerate(cam_name):
         i = i[0]
-        if results[i]['subject_subtype'] == 'camera_trap':
-            subject_id = results[i]['id']
-            manu_id = results[i]['name']
-            print("name: ", manu_id, " id: ", subject_id)
 
-            url_3 = url + subject_id + '/sources/'
-            response = requests.get(url_3, headers=hdr, timeout=20)
-            response_json = response.json()
-            # clear after every get request
-            url_3 = ''
+        # get subject_id from camera name 
+        temp_url = url_2 + cam_name[i]
+        response = requests.get(temp_url, headers=hdr, timeout=10)
+        response_json = response.json()
+        # clear after every request
+        temp_url = ''
+        subject_id = response_json['data'][0]['id']
 
-            if (response_json['status']['message'] != 'Not Found'
-                    and response_json['data'] != []):
-                post_observation(subject_id, "", formatted_time, hdr)
-            else:
-                print("Invalid subject ID. The subject was not found or "
-                      "contained no data.")
+        # check that a source exists for the subject
+        url_3 = url + subject_id + '/sources/'
+        response = requests.get(url_3, headers=hdr, timeout=20)
+        response_json = response.json()
+        # clear after every get request
+        url_3 = ''
+        
+        print("Camera name: ", cam_name[i], "Subject_id: ", subject_id)
+
+        # check if a valid camera subject/source
+        if (response_json['status']['message'] != 'Not Found'
+                and response_json['data'] != []):
+            post_observation(subject_id, "", formatted_time, hdr)
+        else:
+            print("Invalid subject ID. The subject was not found or "
+                    "contained no data.")
