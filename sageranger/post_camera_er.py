f43112f783d55fdb746d9bce8ff7c15a926d6c89
Montserrat Jara <jaramontse3@gmail.com>
2026-06-11 10:18:32 -0700

    Fix bad indentation and pylint errors.

--- sageranger/post_camera_er.py
+++ sageranger/post_camera_er.py
@@ -30,7 +30,7 @@ import requests
 import pandas as pd
 from post_obs import post_observation
 
-def post_camera():
+def post_camera():  # pylint: disable=too-many-locals
     """Adds Cameras to csv list of camera traps"""
 
     auth = input('input authorization: ')
@@ -51,65 +51,65 @@ def post_camera():
     longi = df.longi.tolist()
 
     for i in enumerate(cam):
-      i = i[0]
-      current_time = datetime.now(UTC)
-      formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
+        i = i[0]
+        current_time = datetime.now(UTC)
+        formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f') + 'Z'
 
-      # first create a source
-      payload = {
-          "source_type": "seismic",
-          "manufacturer_id": cam[i],
-          "model_name": cam[i],
-          "additional": {},
-          "provider": "cougar_vision",
-          "subject": {
-              "name": cam[i],
-              "subject_subtype": "camera_trap"
-          },
-          "assigned_range": {}
-      }
+        # first create a source
+        payload = {
+            "source_type": "seismic",
+            "manufacturer_id": cam[i],
+            "model_name": cam[i],
+            "additional": {},
+            "provider": "cougar_vision",
+            "subject": {
+                "name": cam[i],
+                "subject_subtype": "camera_trap"
+            },
+            "assigned_range": {}
+        }
 
-      url_2 = URL + 'sources/'
-      source = requests.post(url_2, headers=hdr, json=payload, timeout=10)
-      response_js = source.json()
-      source_id = response_js['data']['id']
+        url_2 = url + 'sources/'
+        source = requests.post(url_2, headers=hdr, json=payload, timeout=10)
+        response_js = source.json()
+        source_id = response_js['data']['id']
 
-      # Then create a subject
-      payload = {
-          "content_type": "observations.subject",
-          "name": cam[i],
-          "subject_type": "stationary-object",
-          "subject_subtype": "camera_trap",
-          "additional": {},
-          "created_at": formatted_time,
-          "updated_at": formatted_time,
-          "is_active": 1,
-          }
+        # Then create a subject
+        payload = {
+            "content_type": "observations.subject",
+            "name": cam[i],
+            "subject_type": "stationary-object",
+            "subject_subtype": "camera_trap",
+            "additional": {},
+            "created_at": formatted_time,
+            "updated_at": formatted_time,
+            "is_active": 1,
+            }
 
-      url_3 = URL + 'subjects/'
-      subject = requests.post(url_3, headers=hdr, json=payload, timeout=10)
-      subject_js = subject.json()
-      subject_id = subject_js['data']['id']
+        url_3 = url + 'subjects/'
+        subject = requests.post(url_3, headers=hdr, json=payload, timeout=10)
+        subject_js = subject.json()
+        subject_id = subject_js['data']['id']
 
-      # after subject is created add location
-      payload = {
-          "assigned_range": {},
-          "source": source_id,
-          "source_type": "tracking-device",
-          "additional": {},
-          "location": {
-              "latitude": lat[i],
-              "longitude": longi[i]}
-      }
+        # after subject is created add location
+        payload = {
+            "assigned_range": {},
+            "source": source_id,
+            "source_type": "tracking-device",
+            "additional": {},
+            "location": {
+                "latitude": lat[i],
+                "longitude": longi[i]}
+        }
 
-      url_4 = URL + 'subject/' + subject_id+'/sources/'
-      requests.post(url_4, headers=hdr, json=payload, timeout=10)
-      
-      response = requests.get(url_4, headers=hdr, timeout=10)
-      source_2 = response.json()
+        url_4 = url + 'subject/' + subject_id+'/sources/'
+        requests.post(url_4, headers=hdr, json=payload, timeout=10)
 
-      post_observation(subject_id, "", formatted_time, hdr)
+        response = requests.get(url_4, headers=hdr, timeout=10)
+        source_2 = response.json()
 
-      print("\nsubject id: " + subject_id)
-      print("source id: " + source_2['data'][0]['id'])
-      print("camera trap " + cam[i] + " is uploaded to sagebrush\n")\
+        post_observation(subject_id, "", formatted_time, hdr)
+
+        print("\nsubject id: " + subject_id)
+        print("source id: " + source_2['data'][0]['id'])
+        print("camera trap " + cam[i] + " is uploaded to sagebrush\n")
