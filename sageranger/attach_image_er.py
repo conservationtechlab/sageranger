a0882e57cb1358a07b58d3a10ee29f05586bb717
Montserrat Jara <jaramontse3@gmail.com>
2026-06-16 10:00:26 -0700

    Add datatypes to docstrings.

--- sageranger/attach_image_er.py
+++ sageranger/attach_image_er.py
@@ -13,16 +13,19 @@ def attach_image(event_id, img_byte, authorization, label):
     ranger as an alert system.
 
     Args:
-    event_id: the unique id provided by Earthranger to be able to associate
-        an image here with an event alert previously created for for each
-        image to be added
-    img_byte: the image to be posted in binary because that is how ER accepts it
-    authorization:the other auth token for Earthranger as defined in config yml
-    , this was retrieved from the interactive api on ER
-        https://<YOUR INSTANCE>.pamdas.org/api/v1.0/docs/interactive/
-    label: the name of the animal identified to be sent, used as title of jpeg
+        event_id (str): the unique id provided by Earthranger to be able to 
+            associate an image here with an event alert previously created 
+            for for each image to be added
+        img_byte (bytes): the image to be posted in binary because that is how ER 
+            accepts it
+        authorization (str): the auth token for Earthranger as defined in config yml
+            this was retrieved from the interactive api on ER
+            https://<YOUR INSTANCE>.pamdas.org/api/v1.0/docs/interactive/
+        label: the name of the animal identified to be sent, used as title of jpeg
 
-    Returns:the http request response code to tell us if the call worked or not
+    Returns:
+        int: the http request response code to tell us if the call 
+            worked or not
     """
 
     url_var = 'https://sagebrush.pamdas.org/api/v1.0/activity/event/'
