6373f364c579d59a1016304c57079807fc860f52
Montserrat Jara <jaramontse3@gmail.com>
2026-07-09 09:36:46 -0700

    Add class for new config file.

---
+++ sageranger/sensor_class.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class SensorInfo:
+    path_csv:str
+    auth_token: str
+    source_type: str
+    provider: str
+    subject_subtype: str
+    subject_type: str
+    content_type: str
+    source_type: str
+    group_name: str
+
+
+
+
