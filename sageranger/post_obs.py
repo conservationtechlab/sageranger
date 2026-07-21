62ba0d0c03ced3deab1080f0cce0f13046ba016d
Montserrat Jara <jaramontse3@gmail.com>
2026-07-21 13:05:01 -0700

    Update to work with temperature sensors,

--- sageranger/post_obs.py
+++ sageranger/post_obs.py
@@ -8,6 +8,8 @@ and post_monthly.
 """
 
 import requests
+from unpack_info import get_config_info
+from sensor_class import SensorInfo
 
 
 def post_observation(subject_id, label, time, hdr):
@@ -26,15 +28,15 @@ def post_observation(subject_id, label, time, hdr):
             https://<YOUR INSTANCE>.pamdas.org/api/v1.0/docs/interactive/ )
 
     """
+    config = get_config_info(SensorInfo)
 
     url = 'https://sagebrush.pamdas.org/api/v1.0/'
 
     url_3 = url + 'subject/' + subject_id + '/sources/'
     response = requests.get(url_3, headers=hdr, timeout=10)
     response_json = response.json()
-
     source_id = response_json['data'][0]['id']
-    payload = {
+    payload_camera = {
         "location": {
             "longitude": 0,
             "latitude": 0},
@@ -44,6 +46,40 @@ def post_observation(subject_id, label, time, hdr):
             [{"value": "test", "label": "animal", "units": ""}],
         "additional": {"animal": label}
         }
+
+    payload_temp = {
+        "location": {
+            "longitude": 0,
+            "latitude": 0},
+        "recorded_at": time,
+        "source": source_id,
+        "device_status_properties": [{
+            "value": "",
+            "label": "int_temperature",
+            "units": "C"
+            },
+            {"value": "",
+             "label": "ext_temperature",
+             "units": "C"},
+            {"value": "",
+             "label": "humidity",
+             "units": "%"},
+            {"value": "",
+             "label": "bat_stat",
+             "units": "V"}],
+        "additional": {
+            "int_temperature": "",
+            "ext_temperature": "",
+            "humidity": "",
+            "bat_stat": ""
+            }
+     }
+
+    if config.sensor_type:
+        payload = payload_camera
+    else:
+        payload = payload_temp
+
     url_5 = url + 'observations/'
     obs = requests.post(url_5, headers=hdr, json=payload, timeout=20)
     print("Observation response: ", obs)
