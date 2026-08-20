6310c2f94a58c5b7f89e4149175c4e25525846b5
Montserrat Jara <jaramontse3@gmail.com>
2026-08-20 10:48:31 -0700

    Remove redunant post_obs call.

--- sageranger/post_event_er.py
+++ sageranger/post_event_er.py
@@ -3,10 +3,8 @@ Post Event ER This module defines a function called post_event which creates a
 report in Earthranger that shows up on the map and returns the id that event.
 """
 
-from datetime import datetime
 import requests
 from sageranger.get_cam_location import cam_location
-from sageranger.post_obs import post_observation
 
 
 def post_event(label, cam_name, authorization):
@@ -38,9 +36,7 @@ def post_event(label, cam_name, authorization):
     cam, subject_id = cam_location(cam_name, authorization)
     lat = cam[1]
     longi = cam[0]
-
-    current_time = datetime.now()
-    formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    print("LAT LONG ?!?!?!", lat, longi)
 
     event_data = {
         "event_type": "cougarvision_detection",
@@ -67,8 +63,5 @@ def post_event(label, cam_name, authorization):
 
     response_json = new_event.json()
     print(response_json['data']['id'])
-
-    # post observation
-    post_observation(subject_id, label, formatted_time, headers)
 
     return response_json['data']['id']
